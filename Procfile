6f7cc3abfad9eb68af6d34e0d7efa96b07b15574
Benjamin Bishop <benjaminbishop12@gmail.com>
2020-03-04 16:22:04 -0700

    changing Procfile to server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn DATA-SCIENCE:APP -t 120
+web: gunicorn server:APP -t 120
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from airbnb_api import app
+from airbnb_api.app import app
 if __name__ == "__main__":
     app.run()
 # set FLASK_APP=airbnb_api:app; flask run
